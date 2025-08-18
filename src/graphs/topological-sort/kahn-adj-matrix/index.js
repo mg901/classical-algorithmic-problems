5c945c912ea3d3232e6742ff0ed8c46b4b77cacd
Maxim Alyoshin <mgreen097@gmail.com>
2025-08-18 22:18:45 +0300

    refactor(graph):  improve kahn-adj-matrix

--- src/graphs/topological-sort/kahn-adj-matrix/index.js
+++ src/graphs/topological-sort/kahn-adj-matrix/index.js
@@ -1,17 +1,21 @@
-export function topologicalSort(graph) {
-  const n = graph.length;
+export function topologicalSort(matrix) {
+  const n = matrix.length;
   const inDegree = new Uint8Array(n);
   const queue = [];
   const result = [];
 
   for (let u = 0; u < n; u += 1) {
     for (let v = 0; v < n; v += 1) {
-      if (graph[u][v] === 1) inDegree[v] += 1;
+      if (matrix[u][v] === 1) {
+        inDegree[v] += 1;
+      }
     }
   }
 
-  for (let i = 0; i < n; i += 1) {
-    if (inDegree[i] === 0) queue.push(i);
+  for (let v = 0; v < n; v += 1) {
+    if (inDegree[v] === 0) {
+      queue.push(v);
+    }
   }
 
   while (queue.length) {
@@ -19,13 +23,15 @@ export function topologicalSort(graph) {
     result.push(u);
 
     for (let v = 0; v < n; v += 1) {
-      if (graph[u][v] === 1) {
+      if (matrix[u][v] === 1) {
         inDegree[v] -= 1;
 
-        if (inDegree[v] === 0) queue.push(v);
+        if (inDegree[v] === 0) {
+          queue.push(v);
+        }
       }
     }
   }
 
-  return result.length === graph.length ? result : [];
+  return result.length === matrix.length ? result : [];
 }
