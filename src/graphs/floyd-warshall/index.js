ea4771e356a0b15a7639e22e268c8211ee00b681
Maxim Alyoshin <mgreen097@gmail.com>
2025-08-31 22:49:05 +0300

    feat(graphs): add `Floyd-Warshall` Algorithm

--- src/graphs/floyd-warshall/index.js
+++ src/graphs/floyd-warshall/index.js
@@ -1,15 +1,19 @@
+// Explanation:
+// - Abdul Bari: https://youtu.be/oNI0rf2P9gE?si=WKT-u8LajiluOQ3o
+
+// Complexity:
+// - Time: O(V^3)
+// - Space: (V^2)
 export function floydWarshall(graph) {
   const n = graph.length;
   const dist = Array.from({ length: n }, (_, i) =>
-    Array.from({ length: n }, (_, j) => graph[i][j]),
+    Array.from({ length: n }, (__, j) => graph[i][j]),
   );
 
   for (let k = 0; k < n; k += 1) {
     for (let i = 0; i < n; i += 1) {
       for (let j = 0; j < n; j += 1) {
-        if (dist[i][k] !== Infinity && dist[k][j] !== Infinity) {
-          dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
-        }
+        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
       }
     }
   }
